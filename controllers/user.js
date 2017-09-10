574cd2d2dacf5899678d9071081163102bbbea17
Pablo Luis Reinoso <pabloreinoso0@gmail.com>
2017-09-10 14:26:26 -0400

    subir avatar de usuario

--- controllers/user.js
+++ controllers/user.js
@@ -1,6 +1,7 @@
 'use strict';
 //modulos
 const bcrypt = require('bcrypt-nodejs');
+const fs = require('fs');
 
 //modelos
 const User = require('../models/user');
@@ -122,9 +123,62 @@ function updateUser(req, res) {
 
 }
 
+function uploadImage(req, res) {
+    const userId=req.params.id;
+    var file_name='No subido...';
+
+
+    if (req.files.image){
+        var file_path=req.files.image.path;
+        var file_split=file_path.split('/');
+        file_name=file_split[2];
+        var ext_split=file_name.split('.');
+        var file_ext=ext_split[1];
+        if (file_ext == 'png' || file_ext == 'jpg' ||file_ext == 'jpeg' ||file_ext == 'gif'){
+
+            if (userId != req.user.sub){
+                return res.status(500).send({message:'no tienes permiso para actualizar el usuario'})
+            }
+
+            User.findByIdAndUpdate(userId,{image:file_name},{new:true}, (err, userUpdated)=>{
+                if(err){
+                    res.status(500).send({message:'Error al actualizar el usuario'})
+                }else {
+                    if (!userUpdated){
+                        res.status(404).send({message:'no se a podido actualizar el usuario'})
+                    }else {
+                        res.status(200).send({user:userUpdated, image:file_name});
+                    }
+
+                }
+            });
+
+        }else {
+            fs.unlink(file_path, (err)=>{
+                if(err){
+                    res.status(200).send({message:'Extension no validad y fichero no borrado' });
+                }else {
+                    res.status(200).send({message:'Extension no validad' });
+                }
+            });
+
+        }
+
+        // res.status(200).send({
+        //     file_path:file_path,
+        //     file_split:file_split,
+        //     file_name:file_name,
+        //     file_ext:file_ext
+        // });
+    }else {
+        res.status(200).send({message:'no se han subido archivos' });
+    }
+}
+
 module.exports = {
     pruebas,
     saveUser,
     login,
-    updateUser
+    updateUser,
+    uploadImage
 };
