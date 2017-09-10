55e7078bd4b481b39b189d3d4c61d719d4a01ac2
Pablo Luis Reinoso <pabloreinoso0@gmail.com>
2017-09-10 12:18:04 -0400

    Actualizar Usuario

--- controllers/user.js
+++ controllers/user.js
@@ -99,8 +99,32 @@ function login(req, res) {
 
 })}
 
+function updateUser(req, res) {
+
+    const userId= req.params.id;
+    const update=req.body;
+    if (userId != req.user.sub){
+        return res.status(500).send({message:'no tienes permiso para actualizar el usuario'})
+    }
+    User.findByIdAndUpdate(userId,update,{new:true}, (err, userUpdated)=>{
+       if(err){
+           res.status(500).send({message:'Error al actualizar el usuario'})
+       }else {
+           if (!userUpdated){
+               res.status(404).send({message:'no se a podido actualizar el usuario'})
+           }else {
+               res.status(200).send({user:userUpdated});
+           }
+
+       }
+    });
+
+
+}
+
 module.exports = {
     pruebas,
     saveUser,
-    login
+    login,
+    updateUser
 };
--- routes/user.js
+++ routes/user.js
@@ -8,4 +8,5 @@ const md_auth=require('../middlewares/authenticated');
 api.get('/pruebas-del-controlador', md_auth.ensureAuth, UserController.pruebas);
 api.post('/register', UserController.saveUser);
 api.post('/login', UserController.login);
+api.put('/update-user/:id',md_auth.ensureAuth, UserController.updateUser);
 module.exports=api;
