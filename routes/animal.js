7325b5bc7698515ebfef93c250b924267874b565
Pablo Luis Reinoso <pabloreinoso0@gmail.com>
2017-09-10 16:37:59 -0400

    middleware para rol de administrador

--- routes/animal.js
+++ routes/animal.js
@@ -4,17 +4,18 @@ const AnimalController = require('../controllers/animal');
 
 const api = express.Router();
 const md_auth=require('../middlewares/authenticated');
+const md_admin=require('../middlewares/is_admin');
 
 const multipart= require('connect-multiparty');
 const md_upload= multipart({uploadDir:'./uploads/animals'});
 
 api.get('/pruebas-animales', md_auth.ensureAuth, AnimalController.pruebas);
-api.post('/animal', md_auth.ensureAuth, AnimalController.saveAnimal);
+api.post('/animal', [md_auth.ensureAuth,md_admin.isAdmin], AnimalController.saveAnimal);
 api.get('/animals',AnimalController.getAnimals);
 api.get('/animal/:id',AnimalController.getAnimal);
-api.put('/animal/:id',md_auth.ensureAuth,AnimalController.updateAnimal);
-api.post('/upload-image-animal/:id',[md_auth.ensureAuth, md_upload], AnimalController.uploadImage);
+api.put('/animal/:id',[md_auth.ensureAuth,md_admin.isAdmin],AnimalController.updateAnimal);
+api.post('/upload-image-animal/:id',[md_auth.ensureAuth,md_admin.isAdmin, md_upload], AnimalController.uploadImage);
 api.get('/get-image-animal/:imageFile', AnimalController.getImageFile);
-api.delete('/animal/:id',md_auth.ensureAuth,AnimalController.deleteAnimal);
+api.delete('/animal/:id',[md_auth.ensureAuth,md_admin.isAdmin],AnimalController.deleteAnimal);
 
 module.exports=api;
